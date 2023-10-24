bf5692a37d059bd302ba4db4501cc20a0ba33ea4
yesaroun <akor1@naver.com>
2023-10-24 21:40:27 +0900

    first commit

---
+++ src/routes/NotFound.tsx
@@ -0,0 +1,16 @@
+import { Text, Heading, VStack, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+    <VStack bg="gray.100" justifyContent={"center"} minH="100vh">
+      <Heading>Page not found.</Heading>
+      <Text>It seems that your're lost.</Text>
+      <Link to="/">
+        <Button colorScheme={"red"} variant={"link"}>
+          Go home &rarr;{" "}
+        </Button>
+      </Link>
+    </VStack>
+  );
+}
