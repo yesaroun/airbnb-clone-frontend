fd28f74c6353b25a544339976f52436f140c8add
yesaroun <akor1@naver.com>
2023-11-18 21:56:08 +0900

    [feat] login modal

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,8 +1,25 @@
 import { Link, Outlet } from "react-router-dom";
-import { Box, HStack, Button } from "@chakra-ui/react";
-import { FaAirbnb } from "react-icons/fa";
+import {
+  Box,
+  Button,
+  HStack,
+  IconButton,
+  ModalContent,
+  ModalOverlay,
+  useDisclosure,
+  Modal,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  VStack,
+  InputLeftElement,
+  InputGroup,
+  Input,
+} from "@chakra-ui/react";
+import { FaAirbnb, FaLock, FaMoon, FaUserNinja } from "react-icons/fa";
 
 export default function Root() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Box>
       <HStack
@@ -17,9 +34,48 @@ export default function Root() {
           </Link>
         </Box>
         <HStack spacing={2}>
-          <Button>Log in</Button>
+          <IconButton
+            aria-label="Toggle dark mode"
+            variant={"ghost"}
+            icon={<FaMoon />}
+          />
+          <Button onClick={onOpen}>Log in</Button>
           <Button colorScheme={"red"}>Sign up</Button>
         </HStack>
+        <Modal onClose={onClose} isOpen={isOpen}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Log in</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <VStack>
+                <InputGroup size={"md"}>
+                  <InputLeftElement
+                    children={
+                      <Box color="gray.500">
+                        <FaUserNinja />
+                      </Box>
+                    }
+                  />
+                  <Input variant={"filled"} placeholder="Username" />
+                </InputGroup>
+                <InputGroup>
+                  <InputLeftElement
+                    children={
+                      <Box color="gray.500">
+                        <FaLock />
+                      </Box>
+                    }
+                  />
+                  <Input variant={"filled"} placeholder="Password" />
+                </InputGroup>
+              </VStack>
+              <Button mt={4} colorScheme={"red"} w="100%">
+                Log in
+              </Button>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
       </HStack>
       <Outlet></Outlet>
     </Box>
