7a71f60b7faa9e8f99c671cc42fb6e0fa8018e1f
yesaroun <akor1@naver.com>
2023-11-27 00:06:05 +0900

    [FEAT] room grid

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,40 @@
+import { Text, Grid, VStack, Box, Image, HStack } from "@chakra-ui/react";
+import { FaStar } from "react-icons/fa";
+
 export default function Home() {
-    return <span>home</span>
+  return (
+    <Grid
+      mt={10}
+      px={40}
+      columnGap={4}
+      rowGap={8}
+      templateColumns={"repeat(5, 1fr)"}
+    >
+      <VStack alignItems={"flex-start"}>
+        <Box overflow={"hidden"} mb={3} rounded="3xl">
+          <Image
+            h="280"
+            src="https://a0.muscache.com/im/pictures/miso/Hosting-926638381418918258/original/d2cb9e14-50e5-481a-87d0-23b72bb0964b.jpeg?im_w=1200"
+          />
+        </Box>
+        <Box>
+          <Grid gap={2} templateColumns={"6fr 1fr"}>
+            <Text display={"block"} as="b" noOfLines={1} fontSize="md">
+              한국 서울
+            </Text>
+            <HStack spacing={1}>
+              <FaStar size={15} />
+              <Text>5.0</Text>
+            </HStack>
+          </Grid>
+          <Text fontSize={"sm"} color="gray.600">
+            Seoul, S. Korea
+          </Text>
+        </Box>
+        <Text fontSize={"sm"} color="gray.600">
+          <Text as="b">$72</Text> / night
+        </Text>
+      </VStack>
+    </Grid>
+  );
 }
