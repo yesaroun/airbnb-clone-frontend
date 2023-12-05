9c7ccfbec736b3f2d1b81f742308294f45d706a1
yesaroun <akor1@naver.com>
2023-12-06 02:04:40 +0900

    [FEAT] reviews comment

--- src/routes/RoomDetail.tsx
+++ src/routes/RoomDetail.tsx
@@ -6,6 +6,7 @@ import { IReview, IRoomDetail } from "../types";
 import {
   Avatar,
   Box,
+  Container,
   Grid,
   GridItem,
   HStack,
@@ -85,7 +86,7 @@ const RoomDetail = () => {
           />
         </HStack>
         <Box mt={10}>
-          <Heading fontSize={"2xl"}>
+          <Heading mb={5} fontSize={"2xl"}>
             <HStack>
               <FaStar /> <Text>{data?.rating}</Text>
               <Text>・</Text>
@@ -95,6 +96,29 @@ const RoomDetail = () => {
               </Text>
             </HStack>
           </Heading>
+          <Container mt={16} maxW="container.lg" marginX="none">
+            <Grid gap={10} templateColumns={"1fr 1fr"}>
+              {reviewsData?.map((review, index) => (
+                <VStack alignItems={"flex-start"} key={index}>
+                  <HStack>
+                    <Avatar
+                      name={review.user.name}
+                      src={review.user.avatar}
+                      size="md"
+                    />
+                    <VStack spacing={0} alignItems={"flex-start"}>
+                      <Heading fontSize={"md"}>{review.user.name}</Heading>
+                      <HStack spacing={1}>
+                        <FaStar size="12px" />
+                        <Text>{review.rating}</Text>
+                      </HStack>
+                    </VStack>
+                  </HStack>
+                  <Text>{review.payload}</Text>
+                </VStack>
+              ))}
+            </Grid>
+          </Container>
         </Box>
       </Box>
     </>
