4bf91d5860ac8059b2e51ce48fd790da93c50d6a
yesaroun <akor1@naver.com>
2023-12-09 17:50:32 +0900

    [FEAT] login form

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -15,20 +15,36 @@ import {
 import React from "react";
 import { FaLock, FaUserNinja } from "react-icons/fa";
 import SocialLogin from "./SocialLogin";
+import { useForm } from "react-hook-form";
 
 interface LoginModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
+interface IForm {
+  username: string;
+  password: string;
+}
+
 const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>();
+
+  const onSubmit = (data: IForm) => {
+    console.log(data);
+  };
+
   return (
     <Modal onClose={onClose} isOpen={isOpen}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Log in</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
+        <ModalBody as="form" onSubmit={handleSubmit(onSubmit)}>
           <VStack>
             <InputGroup size={"md"}>
               <InputLeftElement
@@ -38,7 +54,14 @@ const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
                   </Box>
                 }
               />
-              <Input variant={"filled"} placeholder="Username" />
+              <Input
+                isInvalid={Boolean(errors.username?.message)}
+                {...register("username", {
+                  required: "Please write a username",
+                })}
+                variant={"filled"}
+                placeholder="Username"
+              />
             </InputGroup>
             <InputGroup size="md">
               <InputLeftElement
@@ -48,10 +71,18 @@ const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
                   </Box>
                 }
               />
-              <Input variant={"filled"} placeholder="Password"></Input>
+              <Input
+                isInvalid={Boolean(errors.password?.message)}
+                {...register("password", {
+                  required: "Please write a password",
+                })}
+                type="password"
+                variant={"filled"}
+                placeholder="Password"
+              ></Input>
             </InputGroup>
           </VStack>
-          <Button mt={4} colorScheme={"red"} w="100%">
+          <Button type="submit" mt={4} colorScheme={"red"} w="100%">
             Log in
           </Button>
           <SocialLogin />
--- src/lib/useUser.ts
+++ src/lib/useUser.ts
@@ -6,6 +6,7 @@ export default function useUser() {
     queryKey: ["me"],
     queryFn: getMe,
     retry: false,
+    refetchOnWindowFocus: false,
   });
   return {
     userLoading: isLoading,
